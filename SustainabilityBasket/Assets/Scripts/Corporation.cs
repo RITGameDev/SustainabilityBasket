c502065e7388984ca491ed9e06fdc67cd4bc9af6
Caleb Katzenstein <caleb.katzenstein@gmail.com>
2019-04-13 16:50:26 -0400

    Created Corporation class

---
+++ SustainabilityBasket/Assets/Scripts/Corporation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Corporation : ScriptableObject
+{
+    new string name;
+    int power;
+    int aqi;
+    int cost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
